5637d54af98d971cd6345c8f1427d351d773a4af
Fathi-Dz <fathi0504@student.abudzar.sch.id>
2026-04-10 10:40:20 +0700

    finish

--- src/UpdateTodo.jsx
+++ src/UpdateTodo.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { updateTodo } from "./todoListSlice";
+import { updateTodo } from "./ToDoSliceSlice";
 import { useNavigate, useParams } from "react-router-dom";
 
 export default function UpdateTodo() {
